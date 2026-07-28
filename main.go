65e020a43a58824cda906488f9ec89df94bc0345
Emmanuel <akomdoemmanuel22@gmail.com>
2026-07-28 10:05:42 +0000

    ticket app

--- main.go
+++ main.go
@@ -11,6 +11,25 @@ func main(){
 	fmt.Printf("Hello, you are welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v tickets remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
-	
+
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("How many tickets do you want to purchase: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Hi %v %v")
 
 }
