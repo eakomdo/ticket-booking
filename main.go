07d9211ba25b363d0ed7f2d66362aae81e57d9c3
Emmanuel <akomdoemmanuel22@gmail.com>
2026-07-28 00:07:09 +0000

    ticket app

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("")
-	fmt.Print("Hello, world!")
+
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int= 50
+	var remainingTickets int = 50
+
+	fmt.Printf("Hello, you are welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v tickets remaining\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+	
 
 }
