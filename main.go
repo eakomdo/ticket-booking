e583e4895a053aac60bc5c88c1fcd2c70c73a42e
Emmanuel <akomdoemmanuel22@gmail.com>
2026-07-28 12:34:15 +0000

    ticket app

--- main.go
+++ main.go
@@ -16,41 +16,46 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v tickets remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+	for {
+
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets int
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
 
-    // collect user data
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		// collect user data
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
 
-	fmt.Println("How many tickets do you want to purchase: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("How many tickets do you want to purchase: ")
+		fmt.Scan(&userTickets)
 
-	isvalidName = len(firstName) >= 2 && len(lastName) >= 2
-	isvalidEmail = strings.Contains(email, "@")
-	isvalidTicketNumber = userTickets > 0 || userTickets <= remainingTickets
+		var isvalidName = len(firstName) >= 2 && len(lastName) >= 2
+		var isvalidEmail = strings.Contains(email, "@")
+		var isvalidTicketNumber = userTickets > 0 || userTickets <= remainingTickets
 
 
-	if isvalidName && isvalidEmail && isvalidTicketNumber{
-		// ask user for number of tickets 
-		remainingTickets = conferenceTickets - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+		if isvalidName && isvalidEmail && isvalidTicketNumber{
+			// ask user for number of tickets 
+			remainingTickets = conferenceTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		fmt.Printf("Hi %v %v. You're welcome to %v. A confirmation email will be sent to %v. We have %v tickets available now\n", firstName, lastName, conferenceName, email, remainingTickets)
+			fmt.Printf("Hi %v %v. You're welcome to %v. A confirmation email will be sent to %v. We have %v tickets available now\n", firstName, lastName, conferenceName, email, remainingTickets)
 
-		// add the user to bookings after collecting their input
-		fmt.Println("These are the bookings we have now:", bookings)
+			// add the user to bookings after collecting their input
+			fmt.Println("These are the bookings we have now:", bookings)
+			
+		}
+
 		
 	}
-
+	
     
 	
 
