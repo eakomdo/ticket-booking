28f19a717b066c2fc3e2ae71acaca787e30df3e0
Emmanuel <akomdoemmanuel22@gmail.com>
2026-07-28 10:09:34 +0000

    ticket app

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main(){
 
 	var conferenceName string = "Go Conference"
 	const conferenceTickets int= 50
-	var remainingTickets int = 50
+
 
 	fmt.Printf("Hello, you are welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v tickets remaining\n", conferenceTickets, remainingTickets)
